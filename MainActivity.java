7ea0dc49f8a7252e5a9a02b340f383402724b8a5
Aziz2seye <62079941+Aziz2seye@users.noreply.github.com>
2020-04-11 19:07:03 +0000

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,121 @@
+package com.aziz.mylocation;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private TextView textView;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Log.d("qwert"," onCreate");
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                textView.append("\n la = "+location.getAltitude()+" lon = "+location.getLongitude());
+
+                //double la = location.getLatitude();
+                //double lon = location.getLongitude();
+
+                //Log.d("log"," location "+la+" "+lon);
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+                Log.d("qwert"," onProviderDisabled");
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        };
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            Log.d("qwert"," fistCondition");
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                Log.d("qwert"," dans la deuxieme condition");
+                requestPermissions(new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.INTERNET
+                }, 10);
+
+                return;
+            }
+            configureButton();
+        }
+        else
+        {
+            Log.d("qwert"," dans le else");
+            configureButton();
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d("qwert"," onRequestPermissionsResult");
+        switch (requestCode)
+        {
+            case 10: Log.d("qwert"," dans case 10");
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED )
+                    Log.d("qwert"," dans case 10, dans if");
+                    configureButton();
+                return;
+        }
+
+
+    }
+
+    private void configureButton() {
+        Log.d("qwert"," dans configureButton");
+        button.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("MissingPermission")
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this, "j'ai clique", Toast.LENGTH_SHORT).show();
+                locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+            }
+        });
+
+    }
+}
